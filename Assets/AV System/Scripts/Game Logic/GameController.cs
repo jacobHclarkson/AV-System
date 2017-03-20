e36c9ecf426147cb03d80ba268372693b443e19b
JHC <jacobhclarkson@gmail.com>
2017-03-20 12:12:41 +0200

    added button images. started tutorial logic. added A button input.

--- Assets/AV System/Scripts/Game Logic/GameController.cs
+++ Assets/AV System/Scripts/Game Logic/GameController.cs
@@ -19,25 +19,43 @@ public class GameController : MonoBehaviour {
 
     // textures for image display
     public Sprite allButtons;
+    public Sprite buttonA;
+    public Sprite buttonB;
+    public Sprite buttonX;
+    public Sprite buttonY;
 
     // look targets
     public GameObject greenLookTarget;
 
     // move targets
 
+    // messages
+    private string prompt_0 = "Hello! Welcome to the tutorial stage. In this tutorial, you will learn how to move and interact in VR. Press the 'A' button on your controller to continue.";
+    private string prompt_1 = "";
+    private string prompt_2 = "";
+    private string prompt_3 = "";
+    private string prompt_4 = "";
+    private string prompt_5 = "";
+    private string prompt_6 = "";
+
 	// Use this for initialization
 	void Start () {
-        ToggleActive(hud);
-        SetTextDisplay("It's working!");
-        ToggleActive(greenLookTarget);
-        SetImageDisplay(allButtons);
+        LookTutorial();
+        //SetTextDisplay("It's working!");
+        //ToggleActive(greenLookTarget);
+        //SetImageDisplay(allButtons);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Return))
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //{
+        //    ToggleActive(reticle);
+        //}
+        if (Input.GetButtonDown("A Button"))
         {
-            ToggleActive(reticle);
+            Debug.Log("a button pressed");
+
         }
 	}
 
@@ -61,8 +79,22 @@ public class GameController : MonoBehaviour {
     }
 
     // Change content of image display
-    void SetImageDisplay(Sprite tex)
+    void SetImageDisplay(Sprite sprite)
     {
-        image.sprite = allButtons;
+        image.sprite = sprite;
+    }
+
+    // look tutorial
+    void LookTutorial()
+    {
+        ToggleActive(hud);
+        SetTextDisplay(prompt_0);
+        SetImageDisplay(buttonA);
+
+        // when A is pressed, show next message
+        if (Input.GetButtonDown("A Button"))
+        {
+            SetTextDisplay(prompt_2);
+        }
     }
 }
