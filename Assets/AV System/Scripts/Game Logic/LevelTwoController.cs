40877a2e1d5ce2459463cec8a3f91f102c673e39
JHC <jacobhclarkson@gmail.com>
2017-04-17 16:43:10 +0200

    finished puzzle logic. added ceiling

---
+++ Assets/AV System/Scripts/Game Logic/LevelTwoController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTwoController : MonoBehaviour {
+    [SerializeField] GameObject gate;
+    [SerializeField] TableColliderCheck horseTable;
+    [SerializeField] TableColliderCheck dragonTable;
+    [SerializeField] TableColliderCheck lionTable;
+
+    bool solved = false;
+	
+	// Update is called once per frame
+	void Update () {
+        if (!solved)
+        {
+            CheckSolution();
+        }
+
+        if(solved && gate.transform.position.y < 4.0f)
+        {
+            gate.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+        }
+	}
+
+    void CheckSolution()
+    {
+        if(horseTable.objectPlaced && dragonTable.objectPlaced && lionTable.objectPlaced)
+        {
+            solved = true;
+        }
+    }
+}
---
+++ Assets/AV System/Scripts/Game Logic/TableColliderCheck.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TableColliderCheck : MonoBehaviour {
+    [SerializeField] GameObject keyObject;
+    public bool objectPlaced = false;
+
+    void OnTriggerEnter(Collider tableTriggerCollider)
+    {
+        if (tableTriggerCollider.GetComponent<Collider>().name == keyObject.name)
+        {
+            objectPlaced = true;
+        }
+    }
+}
