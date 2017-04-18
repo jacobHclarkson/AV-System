5dc04fefd9755d058ccb44e15ea732820eb0bf6a
JHC <jacobhclarkson@gmail.com>
2017-04-18 12:02:55 +0200

    added cue to gui to show when user is looking at an interactive item

---
+++ Assets/InteractiveCue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractiveCue : MonoBehaviour {
+
+    [SerializeField] VRStandardAssets.Utils.VREyeRaycaster eyeRayCaster;
+    [SerializeField] GameObject cueImage;
+
+	// Update is called once per frame
+	void Update () {
+        if(eyeRayCaster.CurrentInteractible != null)
+        {
+            cueImage.SetActive(true);
+        }
+        else
+        {
+            cueImage.SetActive(false);
+        }
+	}
+}
