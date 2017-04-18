3928bb935f1bcd5986beb1b1f37fdcc2d8bf8c8a
JHC <jacobhclarkson@gmail.com>
2017-04-18 10:42:17 +0200

    created general script to check distance between player and interactive objects to see if the player is in range

---
+++ Assets/ProximityChecker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProximityChecker : MonoBehaviour {
+
+    [SerializeField] GameObject player;
+    [SerializeField] float requiredDistance;
+
+    public bool inProximity = false;
+    float distance;
+
+	// Update is called once per frame
+	void Update () {
+        distance = Vector3.Distance(transform.position, player.transform.position);
+        if(distance <= requiredDistance)
+        {
+            inProximity = true;
+        }
+        else
+        {
+            inProximity = false;
+        }
+	}
+}
