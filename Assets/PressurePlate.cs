4f133ade6b35538acb2285aa9e3ee3aba54953e4
JHC <jacobhclarkson@gmail.com>
2017-04-18 14:44:22 +0200

    added new pure vr level - double button puzzle

---
+++ Assets/PressurePlate.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour {
+
+    [SerializeField] GameObject gate;
+    [SerializeField] ProximityChecker proxChecker;
+
+    public bool on = false;
+    public bool standing = false;
+	
+	void Update () {
+        standing = CheckForPlayer();
+        if (on || standing)
+        {
+            if (transform.position.y > 0.02f)
+            {
+                transform.Translate(Vector3.down * Time.deltaTime);
+            }
+
+            if(gate.transform.position.y < 4.0f)
+            {
+                gate.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+            }
+        }
+
+        if(!on && !standing)
+        {
+            if (transform.position.y < 0.1f)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime);
+            }
+
+            if(gate.transform.position.y > 0f)
+            {
+                gate.transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+            }
+        }
+	}
+
+    bool CheckForPlayer()
+    {
+        if (proxChecker.inProximity)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        on = true;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        on = false;
+    }
+}
