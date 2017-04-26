0ed4b17f440db64d00cafcaf4b9b8f0cd8ec6245
JHC <jacobhclarkson@gmail.com>
2017-04-26 14:05:23 +0200

    added dragon room

--- Assets/AV System/Scripts/Game Logic/LevelTwoController.cs
+++ Assets/AV System/Scripts/Game Logic/LevelTwoController.cs
@@ -8,6 +8,12 @@ public class LevelTwoController : MonoBehaviour {
     [SerializeField] TableColliderCheck lionTable;
 
     bool solved = false;
+    Vector3 gateStartPos;
+
+    void Start()
+    {
+        gateStartPos = gate.transform.position;
+    }
 	
 	void Update () {
         if (!solved)
@@ -15,7 +21,7 @@ public class LevelTwoController : MonoBehaviour {
             CheckSolution();
         }
 
-        if(solved && gate.transform.position.y < 4.0f)
+        if(solved && gate.transform.position.y < gateStartPos.y + 3.9)
         {
             gate.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
         }
