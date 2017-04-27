4f73fd27c8fd6df567089be38667d0395d01184e
JHC <jacobhclarkson@gmail.com>
2017-04-27 14:24:10 +0200

    added inventory idea to dragon temple

---
+++ Assets/PlaceDragonStatue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceDragonStatue : MonoBehaviour {
+
+    [SerializeField] ProximityChecker prox;
+    [SerializeField] MeshRenderer rend;
+    [SerializeField] Inventory playerInventory;
+    [SerializeField] GameObject dragonIdol;
+
+
+    private bool placed = false;
+	
+	// Update is called once per frame
+	void Update () {
+        if (!placed)
+        {
+            if (prox.inProximity && playerInventory.HasItem(dragonIdol))
+            {
+                rend.enabled = true;
+                placed = true;
+            }
+        }		
+	}
+}
