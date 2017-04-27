3a81a4023b55c315b9a486e68b6973a2ace1f89a
JHC <jacobhclarkson@gmail.com>
2017-04-27 12:00:48 +0200

    temple of the lion structure created

---
+++ Assets/AV System/Scripts/Game Logic/HorseController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseController : MonoBehaviour {
+    [SerializeField] GameObject gate1;
+    [SerializeField] GameObject gate2;
+    [SerializeField] PressurePlate pressurePlate1;
+    [SerializeField] PressurePlate pressurePlate2;
+
+	void Update () {
+        if(gate1.transform.position.y >= pressurePlate1.gateStartPosition.y + 3.5 && gate2.transform.position.y >= pressurePlate2.gateStartPosition.y + 3.5)
+        {
+            pressurePlate1.solved = true;
+            pressurePlate2.solved = true; 
+        }
+	}
+}
