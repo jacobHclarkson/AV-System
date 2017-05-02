9a5766bf74c4288394d4b2e169d61d4270667120
JHC <jacobhclarkson@gmail.com>
2017-05-02 14:49:51 +0200

    added audio assets

---
+++ Assets/AV System/Scripts/Game Logic/MessageController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageController : MonoBehaviour {
+
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject textDisplayObject;
+    [SerializeField] Text messageDisplay;
+    [SerializeField] GameObject avCue;
+    [SerializeField] GameObject closeMessageCue;
+
+    public bool showHands = false;
+
+    public string message1 = "Find the Temple of the Horse.";
+    public string message2 = "Explore the Temple of the Horse.";
+    public string message3 = "Find the Temple of the Lion.";
+    public string message4 = "Explore the Temple of the Lion.";
+    public string message5 = "Find the key in the Temple of the Horse.";
+    public string message6 = "Find the Temple of the Dragon.";
+    public string message7 = "Explore the Temple of the Dragon.";
+    public string message8 = "Find the Dragon Idol in the Temple of the Lion.";
+
+    void Start()
+    {
+        // display first message
+        if(!textDisplayObject.activeSelf)
+            ToggleMessage();
+        SetMessage(message1);
+    }
+    
+    void Update()
+    {
+        // check for player toggling message
+        if (Input.GetKeyDown(KeyCode.M) || Input.GetButtonDown("B Button"))
+        {
+            ToggleMessage();
+        }
+    }
+
+    // switch message on/off
+    void ToggleMessage()
+    {
+        textDisplayObject.SetActive(!textDisplayObject.activeSelf);
+        closeMessageCue.SetActive(!closeMessageCue.activeSelf);
+        if (showHands)
+        {
+            avCue.SetActive(!avCue.activeSelf);
+        }
+    }
+
+    public void SetAVCue(bool b)
+    {
+        showHands = b;
+        textDisplayObject.SetActive(true);
+        closeMessageCue.SetActive(true);
+        avCue.SetActive(b);
+    }
+
+    public void ToggleAVCue()
+    {
+        avCue.SetActive(!avCue.activeSelf);
+    }
+
+    public void SetMessage(string message)
+    {
+        messageDisplay.text = message;
+        textDisplayObject.SetActive(true);
+        closeMessageCue.SetActive(true);
+    }
+}
---
+++ Assets/AV System/Scripts/Game Logic/PlaceDragonStatue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceDragonStatue : MonoBehaviour {
+
+    [SerializeField] ProximityChecker prox;
+    [SerializeField] MeshRenderer rend;
+    [SerializeField] Inventory playerInventory;
+    [SerializeField] GameObject dragonIdol;
+    [SerializeField] MessageController messageController;
+
+
+    private bool placed = false;
+	
+	// Update is called once per frame
+	void Update () {
+        if (!placed)
+        {
+            if (prox.inProximity && playerInventory.HasItem(dragonIdol))
+            {
+                messageController.SetAVCue(true);
+                rend.enabled = true;
+                placed = true;
+            }
+            if (prox.inProximity && !playerInventory.HasItem(dragonIdol))
+            {
+                messageController.SetMessage(messageController.message8);
+            }
+        }		
+	}
+}
