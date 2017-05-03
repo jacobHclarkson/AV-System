c88396905e5b56bda34d3f70e6e632591a0de1c4
JHC <jacobhclarkson@gmail.com>
2017-05-03 16:43:46 +0200

    started working on lion puzzle

---
+++ Assets/Rune.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rune : MonoBehaviour {
+
+    [SerializeField] VRStandardAssets.Utils.VRInteractiveItem vrItem;
+    [SerializeField] ProximityChecker prox;
+    [SerializeField] GameObject displayPlane;
+
+    public bool active = false;
+
+    Color startColor;
+
+    void Start()
+    {
+        startColor = displayPlane.GetComponent<Renderer>().material.color;
+    }
+
+    void Update()
+    {
+        // if player close and presses A
+        if(vrItem.IsOver && prox.inProximity && (Input.GetButtonDown("A Button") || Input.GetButtonDown("Jump")))
+        {
+            // play sound
+            ToggleActive();
+        }
+
+        if (active)
+        {
+            // glow
+            displayPlane.GetComponent<Renderer>().material.color = Color.green;
+        }else
+        {
+            displayPlane.GetComponent<Renderer>().material.color = startColor;
+        }
+    }
+
+    void ToggleActive()
+    {
+        active = !active;
+    }
+
+}
